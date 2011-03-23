6df049dc9c67dfc18138c103e94da7e28d98844a
Aleksandar Simic <asimic@gmail.com>
2011-03-23 15:23:43 +0000

    још тестова за Config.title

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -4,9 +4,23 @@ describe "Config" do
   
   describe ".title" do
     it "should return value" do
-       YAML.stub!(:load_file).with("setup.yaml").and_return({"title" => "Naslov"}) 
+      YAML.stub!(:load_file).with("setup.yaml").and_return({"title" => "Naslov"}) 
       Config.title.should match("Naslov")
     end
   end
 
+
+  context "when the title is blank" do
+    it "should return nothing" do
+      YAML.stub!(:load_file).with("setup.yaml").and_return({"title" => ""}) 
+      Config.title.should match("")
+    end
+  end
+
+  context "when the title is missing" do
+    it "should return nil" do
+      YAML.stub!(:load_file).with("setup.yaml").and_return({}) 
+      Config.title.should be_nil
+    end
+  end
 end  
