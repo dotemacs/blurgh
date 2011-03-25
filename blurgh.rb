802619d99ef70396ac45022324538bb9871039c4
Aleksandar Simic <asimic@gmail.com>
2011-03-25 16:42:32 +0000

    урађена метода get_posts

--- blurgh.rb
+++ blurgh.rb
@@ -18,6 +18,27 @@ module Config
   end
 end
 
+def get_posts
+
+  all_posts = Hash.new {
+    |h,k| h[k] = Hash.new(&h.default_proc) 
+  }
+
+  store = Config.store
+  post_dir = File.join(store + "/" + "*.md")
+
+  Dir.glob(post_dir).each do |post|
+    header, body = File.readlines(post, "")
+    data = YAML.load(header)
+    all_posts[data['date']]['url']   = post.gsub("\.md", "").gsub(store + "/", "")
+    all_posts[data['date']]['title'] = data['title']
+    all_posts[data['date']]['body']  = body
+  end
+  
+  all_posts.sort.reverse
+
+end
+
 get '/' do
   @title = Config.title
   erb :index 
--- spec/blurgh_spec.rb
+++ spec/blurgh_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "blurgh" do
@@ -6,26 +7,40 @@ describe "blurgh" do
     @app ||= Sinatra::Application
   end
 
-  before :each do
-    YAML.should_receive(:load_file).with("setup.yaml").and_return({"title" => "Naslov"}) 
-  end
+  context "routes and content" do
+    before :each do
+      YAML.should_receive(:load_file).with("setup.yaml").and_return({"title" => "Naslov", "store" => "posts"}) 
+    end
 
-  it "should respond to /" do
-    get '/'
-    last_response.should be_ok
-  end
-
-  it "should have a title" do
-    get '/'
-    last_response.body.should match("Naslov")
-  end
-
-  context "the view" do
-    it "should have a body html elements" do
+    it "should respond to /" do
       get '/'
-      last_response.body.should match("<body>")
+      last_response.should be_ok
+    end
+
+    it "should have a title" do
+      get '/'
+      last_response.body.should match("Naslov")
+    end
+
+    context "the view" do
+      it "should have a body html elements" do
+        get '/'
+        last_response.body.should match("<body>")
+      end
     end
   end
 
+  describe "get_posts" do
+    it "should return posts" do
+      first_article = File.readlines("spec/fixtures/o-kapadokiji.md", "")[1]
+      second_article = File.readlines("spec/fixtures/let.md", "")[1]
+      YAML.should_receive(:load_file).with("setup.yaml").and_return({"title" => "Naslov", "store" => "spec/fixtures"}) 
+
+      get_posts.should == \
+      [[20110325,  {"url" => "let", "title" => "Авионски лет", "body" => "#{second_article}"}],\
+       [20110324, {"url" => "o-kapadokiji", "title" => "Кападокија", "body" => "#{first_article}"}]]
+
+    end
+  end
 
 end
