a79f8c87d0ca4654fab3415286a7ac84bf692122
Aleksandar Simic <asimic@gmail.com>
2011-04-01 15:57:45 +0100

    смршао blurgh_spec.rb

--- spec/blurgh_spec.rb
+++ spec/blurgh_spec.rb
@@ -3,40 +3,44 @@ require 'spec_helper'
 
 describe "blurgh" do
 
+  before :each do
+    YAML.should_receive(:load_file)\
+      .and_return({"title" => "Naslov", "store" => "spec/fixtures"})
+  end
+
   def app
     @app ||= Sinatra::Application
   end
 
   context "routes and content" do
-    before :each do
-      YAML.should_receive(:load_file)\
-        .with("setup.yaml").and_return({"title" => "Naslov", "store" => "posts"})
-    end
 
     it "should respond to /" do
       get '/'
       last_response.should be_ok
     end
 
-    it "should have a title" do
-      get '/'
-      last_response.body.should match("Naslov")
-    end
-
     context "the view" do
+
+      it "should have a title" do
+        get '/'
+        last_response.body.should match("Naslov")
+      end
+
       it "should have a body html elements" do
         get '/'
         last_response.body.should match("<body>")
       end
+
+      it "the posts titles should be shown" do
+        get '/'
+        last_response.body.should match('<a href="let">Авионски лет</a>')
+        last_response.body.should match('<a href="o-kapadokiji">Кападокија</a>')
+      end
+
     end
   end
 
   describe "get_posts" do
-
-    before do
-      YAML.should_receive(:load_file).with("setup.yaml")\
-        .and_return({"title" => "Naslov", "store" => "spec/fixtures"})
-    end
 
     it "should return posts" do
       first_article = File.readlines("spec/fixtures/o-kapadokiji.md", "")[1]
@@ -48,29 +52,9 @@ describe "blurgh" do
        [20110324, {"url" => "o-kapadokiji", "title" => "Кападокија", "body" => "#{first_article}"}]]
     end
 
-    context "on the index page" do
-      it "the posts URLs should be listed" do
-        get '/'
-        last_response.body.should match("let")
-        last_response.body.should match("o-kapadokiji")
-      end
-
-      it "the posts titles should be shown" do
-        get '/'
-        last_response.body.should match('<a href="let">Авионски лет</a>')
-        last_response.body.should match('<a href="o-kapadokiji">Кападокија</a>')
-      end
-
-    end
-
   end
 
   describe "get_post" do
-
-    before do
-      YAML.should_receive(:load_file).with("setup.yaml")\
-        .and_return({"title" => "Naslov", "store" => "spec/fixtures"})
-    end
 
     it "should return a post in two parts" do
       get_post("o-kapadokiji").should have(2).parts
