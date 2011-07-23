fa817b845bc8cf621fe185a98dde00d804625f4f
Aleksandar Simic <asimic@gmail.com>
2011-07-23 18:03:24 +0100

    cleaner clicky helper

--- blurgh.rb
+++ blurgh.rb
@@ -80,10 +80,10 @@ helpers do
     "<link href=\"feed.xml\" type=\"application/atom+xml\" rel=\"alternate\" title=\"" + @title + "\" />"
   end
 
-  def clicky(id)
+  def clicky
     "<script src=\"http://static.getclicky.com/js\" type=\"text/javascript\"></script>
-     <script type=\"text/javascript\">clicky.init(" + id.to_s + ");</script>
-     <noscript><p><img alt=\"Clicky\" width=\"1\" height=\"1\" src=\"http://in.getclicky.com/" + id.to_s + "ns.gif\" /></p></noscript>"
+     <script type=\"text/javascript\">clicky.init(" + @clicky_id + ");</script>
+     <noscript><p><img alt=\"Clicky\" width=\"1\" height=\"1\" src=\"http://in.getclicky.com/" + @clicky_id + "ns.gif\" /></p></noscript>"
   end
 end
 
